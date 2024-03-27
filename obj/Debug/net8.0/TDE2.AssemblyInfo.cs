55311e54631879159df71a7e351aa8e7554c4fd6
cristianlumertz <cristianlumertz@gmail.com>
2024-03-26 21:23:34 -0300

    adicionado git ignore

--- obj/Debug/net8.0/TDE2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TDE2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDE2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39608585b727be6e8766f10abda38f05dbcf2d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDE2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDE2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
